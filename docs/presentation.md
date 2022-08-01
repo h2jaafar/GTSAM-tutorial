2c786d9e0d9ac486c021f0523d0f06d7324b9496
Hussein <husseinali.jaafar@ryerson.ca>
2022-08-01 17:12:02 -0400

    Added example 3

--- docs/presentation.md
+++ docs/presentation.md
@@ -156,13 +156,335 @@ A visual representation is in the next slide
 #### Modelling Robot Motion
 
 ![bg w:95%](./factor_graph_2.drawio.svg)
-<\br/>
-<\br/>
-<\br/>
+<br/>
+<br/>
+<br/>
 Open `robotMotion.cpp` to follow along
 
 ---
 #### Importing libraries
+<style scoped>section { font-size: 34px; }</style>
 `#include <gtsam/geometry/Pose2.h>`: We will use Pose2 variables (x, y, theta) to represent the robot positions
-`#include <gtsam/slam/BetweenFactor.h>`: 
+`#include <gtsam/slam/BetweenFactor.h>`: Between factors used to describe relative motion between odom measurments
+`#include <gtsam/nonlinear/NonlinearFactorGraph.h>`: Nonlinear factor graph
+`#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>`: MAP Solver
+`#include <gtsam/nonlinear/Marginals.h>`: marginals covariance
+`#include <gtsam/nonlinear/Values.h>`: store initial guesses in values container
 
+---
+
+// Create an empty nonlinear factor graph
+```c++
+NonlinearFactorGraph graph;
+```
+
+Remember, for a factor we need three things:
+  1. A key or set of keys to label the variables
+  2. A measurement value
+  3. A measurement model
+
+---
+<style scoped>section { font-size: 30px; }</style>
+Create prior value of pose 1, setting it to origin
+```c++
+Pose2 priorMean(0.0,0.0,0.0);
+```
+Create prior noise model
+```c++
+noiseModel::Diagonal::shared_ptr priorNoise = 
+  noiseModel::Diagonal::Sigmas(Vector3(0.3, 0.3, 0.1));
+```
+Create a key for this factor (we can use symbols like before, or an int).
+We can now create this factor and add it to the graph
+```c++
+int key_1 = 1;
+graph.add(PriorFactor<Pose2>(key_1, priorMean, priorNoise));
+```
+
+---
+Create odometry factor values and measurment model
+```c++
+gtsam::Pose2 odometry(2.0, 0.0, 0.0);
+noiseModel::Diagonal::shared_ptr odometryNoise = 
+  noiseModel::Diagonal::Sigmas(Vector3(0.2, 0.2, 0.1));
+// for keys we will just use int 2 and int 3
+```
+Add to factor graph
+```c++
+graph.add(BetweenFactor<Pose2>(1, 2, odometry, odometryNoise));
+// we will use the same odom measurment (since its relative motion)
+graph.add(BetweenFactor<Pose2>(2, 3 odometry, odometryNoise));
+```
+---
+<style scoped>section { font-size: 28px; }</style>
+We can print the summary so far
+```
+graph.print("\nFactor Graph:\n"); // print
+```
+Output:
+```bash
+Factor Graph:
+size: 3
+
+Factor 0: PriorFactor on 1
+  prior mean: (0, 0, 0)
+  noise model: diagonal sigmas[0.3; 0.3; 0.1];
+
+Factor 1: BetweenFactor(1,2)
+  measured: (2, 0, 0)
+  noise model: diagonal sigmas[0.2; 0.2; 0.1];
+
+Factor 2: BetweenFactor(2,3)
+  measured: (2, 0, 0)
+  noise model: diagonal sigmas[0.2; 0.2; 0.1];
+```
+
+---
+To optimize, we need initial values (guesses)
+```c++
+  Values initial;
+  initial.insert(1, Pose2(0.5, 0.0, 0.2));
+  initial.insert(2, Pose2(2.3, 0.1, -0.2));
+  initial.insert(3, Pose2(4.1, 0.1, 0.1));
+  initial.print("\nInitial Estimate:\n"); // print
+```
+Optimize using Levenberg-Marquardt optimization
+```c++
+Values result = LevenbergMarquardtOptimizer(graph, initial).optimize();
+```
+
+---
+<style scoped>section { font-size: 25px; }</style>
+Output
+```c++
+result.print("Final Result:\n");
+  ```
+
+```
+Initial Estimate:
+Values with 3 values:
+Value 1: (gtsam::Pose2) (0.5, 0, 0.2)
+
+Value 2: (gtsam::Pose2) (2.3, 0.1, -0.2)
+
+Value 3: (gtsam::Pose2) (4.1, 0.1, 0.1)
+
+Final Result:
+Values with 3 values:
+Value 1: (gtsam::Pose2) (7.46978302e-16, -5.34409093e-16, -1.7838186e-16)
+
+Value 2: (gtsam::Pose2) (2, -1.09236635e-15, -2.48671177e-16) 
+
+Value 3: (gtsam::Pose2) (4, -1.70076055e-15, -2.5094386e-16) 
+```
+recall ground truth is x1 = (0, 0, 0), x2 = (2,0,0), and x3 = (2+2=4, 0, 0)
+
+---
+<style scoped>section { font-size: 25px; }</style>
+We can also calculate the marginal covariance matrix
+```c++
+  cout.precision(2);
+  Marginals marginals(graph, result);
+  cout << "x1 covariance:\n" << marginals.marginalCovariance(1) << endl;
+  cout << "x2 covariance:\n" << marginals.marginalCovariance(2) << endl;
+  cout << "x3 covariance:\n" << marginals.marginalCovariance(3) << endl;
+```
+Result:
+```
+x1 covariance:
+   0.09 2.4e-33 2.8e-33
+2.4e-33    0.09 1.9e-17
+2.8e-33 1.9e-17    0.01
+x2 covariance:
+   0.13 1.2e-18 6.1e-19
+1.2e-18    0.17    0.02
+6.1e-19    0.02    0.02
+x3 covariance:
+   0.17 8.6e-18 2.7e-18
+8.6e-18    0.37    0.06
+2.7e-18    0.06    0.03
+```
+---
+# Questions so far?
+- We have done examples 1 (sngle unary factor and optimization), and example 2 (multiple binary factors)
+- Next is example 3: Robot Localization
+---
+
+## Example 3: Robot Localization
+- The previous example relies on odometry measurments, but they aren't entirely accurate.
+- They also rely on the previous factor, which can lead to error proagation.
+- A more reliable approach is to use unary measurment factors at each continuous variable 
+- This is especially applicable to robot localization
+
+---
+
+![bg w:80%](./factor_graph_localization.drawio.svg)
+
+Open `robotLocalization.cpp` to follow along
+</br>
+</br>
+</br>
+</br>
+- Here we use bullets for factors instead of squares.
+- $x$ are the variables (poses), and $z$ represents the measurement.
+- We will use GPS measurements 
+---
+<style scoped>section { font-size: 30px; }</style>
+
+Pull headers
+
+`#include <gtsam/geometry/Pose2.h>`: Variables will be pose2
+`#include <gtsam/inference/Key.h>`: We will use symbolic keys instead of ints
+`#include <gtsam/slam/BetweenFactor.h>`: Between variable factors
+`#include <gtsam/nonlinear/Values.h>`: Value container to store initial guesses
+`#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>`: MAP solver
+`#include <gtsam/nonlinear/Marginals.h>`: To compute marginal covariance matrix
+`#include <gtsam/nonlinear/NonlinearFactor.h>`: Non linear factor graph
+
+---
+<style scoped>section { font-size: 25px; }</style>
+First we can create a custom class for the unary factors (for GPS measurments). 
+It inherits from `NoiseModelFactor`
+```c++
+class UnaryFactor: public NoiseModelFactor1<Pose2> {
+  double mx_, my_; // define the x and y locations
+public: 
+  // define a shorthand for shared_ptr
+  typedef boost::shared_ptr<UnaryFactor> shared_ptr; 
+
+// create constructor and also pass it the initiated list for the parent class
+UnaryFactor(Key j, double x, double y, const SharedNoiseModel& model):
+  NoiseModelFactor1<Pose2>(model, j), mx_(x), my_(y) {}
+
+~UnaryFactor() override {}
+// override evaluateError and clone functions, since we are using a custom class
+Vector evaluateError(const Pose2& q, boost::optional<Matrix&> H = boost::none) const override {
+    const Rot2& R = q.rotation();
+    if (H) (*H) = (gtsam::Matrix(2, 3) << R.c(), -R.s(), 0.0, R.s(), R.c(), 0.0).finished();
+    return (Vector(2) << q.x() - mx_, q.y() - my_).finished();
+}
+// the math for this is in the code 
+gtsam::NonlinearFactor::shared_ptr clone() const override {
+  return boost::static_pointer_cast<gtsam::NonlinearFactor>(
+      gtsam::NonlinearFactor::shared_ptr(new UnaryFactor(*this))); } };
+```
+
+---
+
+Now main function:
+
+```c++
+int main(int argc, char**argv){
+  // 1. Create a factor graph container and add factors to it
+  NonlinearFactorGraph graph;
+
+  // add odometry factors
+// create noise model
+auto odometryNoise = noiseModel::Diagonal::Sigmas(Vector3(0.2, 0.2, 0.1));
+// add to factor graph (we use direct pose2 definitiions here)
+
+graph.emplace_shared<BetweenFactor<Pose2> >(1, 2, Pose2(2.0, 0.0, 0.0), odometryNoise);
+graph.emplace_shared<BetweenFactor<Pose2> >(2, 3, Pose2(2.0, 0.0, 0.0), odometryNoise);
+
+```
+
+---
+Add GPS measurements
+
+```c++
+// create noise model
+auto unaryNoise =
+      noiseModel::Diagonal::Sigmas(Vector2(0.1, 0.1));  // 10cm std on x,y
+// add the three unary factors to the graph 
+graph.emplace_shared<UnaryFactor>(1, 0.0, 0.0, unaryNoise);
+graph.emplace_shared<UnaryFactor>(2, 2.0, 0.0, unaryNoise);
+graph.emplace_shared<UnaryFactor>(3, 4.0, 0.0, unaryNoise);
+graph.print("\n Factr Graph:\n");
+```
+---
+
+Result of print:
+<style scoped>section { font-size: 25px; }</style>
+```
+Factor Graph:
+size: 5
+
+Factor 0: BetweenFactor(1,2)
+  measured: (2, 0, 0)
+  noise model: diagonal sigmas[0.2; 0.2; 0.1];
+
+Factor 1: BetweenFactor(2,3)
+  measured: (2, 0, 0)
+  noise model: diagonal sigmas[0.2; 0.2; 0.1];
+
+Factor 2:   keys = { 1 }
+isotropic dim=2 sigma=0.1
+
+Factor 3:   keys = { 2 }
+isotropic dim=2 sigma=0.1
+
+Factor 4:   keys = { 3 }
+isotropic dim=2 sigma=0.1
+```
+
+---
+To optimize, we need initial guesses
+```cpp
+Values initialEstimate;
+initialEstimate.insert(1, Pose2(0.5, 0.0, 0.2));
+initialEstimate.insert(2, Pose2(2.3, 0.1, -0.2));
+initialEstimate.insert(3, Pose2(4.1, 0.1, 0.1));
+initialEstimate.print("\nInitial Estimate:\n");  // print
+```
+Results:
+```
+Initial Estimate:
+Values with 3 values:
+Value 1: (gtsam::Pose2) (0.5, 0, 0.2)
+Value 2: (gtsam::Pose2) (2.3, 0.1, -0.2)
+Value 3: (gtsam::Pose2) (4.1, 0.1, 0.1)
+```
+
+---
+<style scoped>section { font-size: 30px; }</style>
+Optimize:
+```cpp
+LevenbergMarquardtOptimizer optimizer(graph, initialEstimate);
+Values result = optimizer.optimize();
+result.print("Final Result:\n");
+```
+Results
+```
+Final Result:
+Values with 3 values:
+Value 1: (gtsam::Pose2) (8.53538039e-11, 2.35867311e-12, 6.46369985e-11)
+Value 2: (gtsam::Pose2) (2, -4.74793416e-12, 5.98996275e-11)
+Value 3: (gtsam::Pose2) (4, 2.38926105e-12, 5.99195006e-11)
+```
+recall ground truth is $x_1 = (0, 0, 0), x_2 = (2,0,0), \text{and } x_3 = (4, 0, 0)$
+
+---
+<style scoped>section { font-size: 25px; }</style>
+
+Covariance
+```
+Marginals marginals(graph, result);
+  cout << "x1 covariance:\n" << marginals.marginalCovariance(1) << endl;
+  cout << "x2 covariance:\n" << marginals.marginalCovariance(2) << endl;
+  cout << "x3 covariance:\n" << marginals.marginalCovariance(3) << endl;
+```
+Result:
+```
+x1 covariance:
+  0.00828571429  7.82228372e-14 -2.06607043e-13
+ 7.82228372e-14   0.00944444444  -0.00305555556
+-2.06607043e-13  -0.00305555556   0.00819444445
+x2 covariance:
+  0.00714285714  3.80266543e-14 -5.38289642e-14
+ 3.80266543e-14   0.00777777778  -0.00111111111
+-5.38289642e-14  -0.00111111111   0.00819444444
+x3 covariance:
+ 0.00828571429 6.60868131e-14 1.73935528e-13
+6.60868131e-14  0.00944444444  0.00305555556
+1.73935528e-13  0.00305555556   0.0181944444
+```
--- src/robotLocalization.cpp
+++ src/robotLocalization.cpp
@@ -1,3 +1,19 @@
+/* ----------------------------------------------------------------------------
+
+ * GTSAM Copyright 2010, Georgia Tech Research Corporation,
+ * Atlanta, Georgia 30332-0415
+ * All Rights Reserved
+ * Authors: Frank Dellaert, et al. (see THANKS for the full author list)
+
+ * See LICENSE for the license information
+
+ * -------------------------------------------------------------------------- */
+
+/**
+ * @file LocalizationExample.cpp
+ * @brief Simple robot localization example, with three "GPS-like" measurements
+ * @author Frank Dellaert
+ */
 
 /**
  * A simple 2D pose slam example with "GPS" measurements
@@ -9,15 +25,12 @@
 
 // We will use Pose2 variables (x, y, theta) to represent the robot positions
 #include <gtsam/geometry/Pose2.h>
-#include <gtsam/geometry/Moses3.h>
 
 // We will use simple integer Keys to refer to the robot poses.
 #include <gtsam/inference/Key.h>
 
 // As in OdometryExample.cpp, we use a BetweenFactor to model odometry measurements.
 #include <gtsam/slam/BetweenFactor.h>
- #include <gtsam/slam/PriorFactor.h>
- #include <gtsam/navigation/GPSFactor.h>
 
 // We add all facors to a Nonlinear Factor Graph, as our factors are nonlinear.
 #include <gtsam/nonlinear/NonlinearFactorGraph.h>
@@ -40,9 +53,7 @@
 #include <gtsam/nonlinear/Marginals.h>
 
 using namespace std;
-using namespace Sophus;
 using namespace gtsam;
-
 
 // Before we begin the example, we must create a custom unary factor to implement a
 // "GPS-like" functionality. Because standard GPS measurements provide information
@@ -54,286 +65,78 @@ using namespace gtsam;
 // the NoiseModelFactor1.
 #include <gtsam/nonlinear/NonlinearFactor.h>
 
-#define PI 3.14159265359f
-#define useUnary 1
-#define printCov 0
-
-
-namespace gtsam{
-
-
-// create a custom class for modified unary factor
-class UnaryFactor: public NoiseModelFactor1<Moses3> {
-
+class UnaryFactor: public NoiseModelFactor1<Pose2> {
   // The factor will hold a measurement consisting of an (X,Y) location
   // We could this with a Point2 but here we just use two doubles
-  Point3 nT_;
+  double mx_, my_;
 
-public:
+ public:
   /// shorthand for a smart pointer to a factor
   typedef boost::shared_ptr<UnaryFactor> shared_ptr;
 
   // The constructor requires the variable key, the (X, Y) measurement value, and the noise model
-  UnaryFactor(Key j, const Point3& gpsIn, const SharedNoiseModel& model):
-    NoiseModelFactor1<Moses3>(model, j), nT_(gpsIn) {}
+  UnaryFactor(Key j, double x, double y, const SharedNoiseModel& model):
+    NoiseModelFactor1<Pose2>(model, j), mx_(x), my_(y) {}
 
-  virtual ~UnaryFactor() {}
+  ~UnaryFactor() override {}
 
   // Using the NoiseModelFactor1 base class there are two functions that must be overridden.
   // The first is the 'evaluateError' function. This function implements the desired measurement
   // function, returning a vector of errors when evaluated at the provided variable value. It
   // must also calculate the Jacobians for this measurement function, if requested.
-
-  Vector evaluateError(const Moses3& q, boost::optional<Matrix&> H = boost::none) const
-  {
-    // The measurement function for a GPS-like measurement is simple:
-    // error_x = pose.x - measurement.x
-    // error_y = pose.y - measurement.y
-    // Consequently, the Jacobians are:
-    // [ derror_x/dx  derror_x/dy  derror_x/dtheta ] = [1 0 0]
-    // [ derror_y/dx  derror_y/dy  derror_y/dtheta ] = [0 1 0]
-    //if (H) (*H) = (Matrix(2,3) << 1.0,0.0,0.0, 0.0,1.0,0.0); //this is crap for moses3
-    //return (Vector(2) << q.x() - m_.x(), q.y() - m_.y());          //this is crap for moses3
-
-    if (H) {
-	    H->resize(3, 7);
-	    H->block < 3, 3 > (0, 0) << q.Sim3::rotation_matrix();  //deriv. trans NOT TESTED
-	    H->block < 3, 3 > (0, 3) << zeros(3,3);				//deriv. rot NOT TESTED
-	    H->block < 3, 1 > (0, 6) << zeros(3, 1);	//6 not 4      		//deriv scale NOT TESTED
-
-
-	  }
-
-	Vector3 res =nT_.localCoordinates(Point3(q.Sim3::translation()));
-	//cout << "****** "<<res.transpose() <<"   ";
-	//cout << nT_.vector().transpose() << "    "<< q.Sim3::translation().transpose() <<" ]]" << endl;
-  	return res;
-
-
+  Vector evaluateError(const Pose2& q, boost::optional<Matrix&> H = boost::none) const override {
+    // The measurement function for a GPS-like measurement h(q) which predicts the measurement (m) is h(q) = q, q = [qx qy qtheta]
+    // The error is then simply calculated as E(q) = h(q) - m:
+    // error_x = q.x - mx
+    // error_y = q.y - my
+    // Node's orientation reflects in the Jacobian, in tangent space this is equal to the right-hand rule rotation matrix
+    // H =  [ cos(q.theta)  -sin(q.theta) 0 ]
+    //      [ sin(q.theta)   cos(q.theta) 0 ]
+    const Rot2& R = q.rotation();
+    if (H) (*H) = (gtsam::Matrix(2, 3) << R.c(), -R.s(), 0.0, R.s(), R.c(), 0.0).finished();
+    return (Vector(2) << q.x() - mx_, q.y() - my_).finished();
   }
 
   // The second is a 'clone' function that allows the factor to be copied. Under most
   // circumstances, the following code that employs the default copy constructor should
   // work fine.
-  virtual gtsam::NonlinearFactor::shared_ptr clone() const {
+  gtsam::NonlinearFactor::shared_ptr clone() const override {
     return boost::static_pointer_cast<gtsam::NonlinearFactor>(
         gtsam::NonlinearFactor::shared_ptr(new UnaryFactor(*this))); }
 
   // Additionally, we encourage you the use of unit testing your custom factors,
   // (as all GTSAM factors are), in which you would need an equals and print, to satisfy the
   // GTSAM_CONCEPT_TESTABLE_INST(T) defined in Testable.h, but these are not needed below.
-
-}; // UnaryFactor
-
+};  // UnaryFactor
 
 
-
-}
 int main(int argc, char** argv) {
-
   // 1. Create a factor graph container and add factors to it
   NonlinearFactorGraph graph;
-  NonlinearFactorGraph graphWithGPS;
-
-
-  //Intermediate constraints
-  Rot3 R0 = Rot3(Rot3::rodriguez(0.0, 0.0, 0.0)); //Initial prior to origin
-  Point3 Pp0(0.0, 0.0, 0.0);  
-  
-
-  int minus = +1;
-
-  Rot3 R12 = Rot3(Rot3::rodriguez(0.0, 0.0, minus*PI/9.0));
-  Point3 Pp12(1.0, 0.0, 0.0);
-
-  Rot3 R23 = Rot3(Rot3::rodriguez(0.0, 0.0, minus*PI/9.0));
-  Point3 Pp23(1.0, 0.0, 0.0);   
-
-  Rot3 R34 = Rot3(Rot3::rodriguez(0.0, 0.0, minus*PI/9.0));
-  Point3 Pp34(1.0, 0.0, 0.0);
-
-
-
-  Rot3 R45 = Rot3(Rot3::rodriguez(0.0, 0.0, minus*PI/9.0));
-  Point3 Pp45(1.0, 0.0, 0.0);
-
-  Rot3 R56 = Rot3(Rot3::rodriguez(0.0, 0.0, minus*PI/9.0));
-  Point3 Pp56(1.0, 0.0, 0.0);   
-
-  Rot3 R67 = Rot3(Rot3::rodriguez(0.0, 0.0, minus*PI/9.0));
-  Point3 Pp67(1.0, 0.0, 0.0);
-
-
-  Rot3 R78 = Rot3(Rot3::rodriguez(0.0, 0.0, minus*PI/9.0));
-  Point3 Pp78(1.0, 0.0, 0.0);
-
-  Rot3 R89 = Rot3(Rot3::rodriguez(0.0, 0.0, minus*PI/9.0));
-  Point3 Pp89(1.0, 0.0, 0.0);   
-
-  Rot3 R90 = Rot3(Rot3::rodriguez(0.0, 0.0, minus*PI/9.0));
-  Point3 Pp90(1.0, 0.0, 0.0);
-
-
-
-  float s0,s12,s23,s34,s45,s56,s67,s78,s89,s90;
-  s0=1; //Initial Prior to origin.
-
-  s12=0.95;
-  s23=0.9;
-  s34=0.85;
-  s45=0.8;
-  s56=0.75;
-  s67=0.7;
-  s78=0.65;
-  s89=0.6;
-  s90=0.55;
-
-
-  //Unary priors
-  Point3 Up1(0, 0, 0.0);  
-  Point3 Up2(1, -3.78165e-17, 0.0);  
-  Point3 Up3(1.93969, 0.34202, 0.0);  
-  Point3 Up4(2.70574, 0.984808, 0.0);  
-  Point3 Up5(3.20574, 1.85083, 0.0);  
-  Point3 Up6(3.37939, 2.83564, 0.0);  
-  Point3 Up7(3.20574, 3.82045, 0.0);  
-  Point3 Up8(2.70574, 4.68647, 0.0);  
-  Point3 Up9(1.93969, 5.32926, 0.0);  
-  Point3 Up10(1, 5.67128, 0.0);  
-
-
-  //Initial Estimate values
-  Point3 Pi1(0.0, 0.0, 0.0);
-  Point3 Pi2(1.0, 1.0, 0.0);
-  Point3 Pi3(2.0, 1.0, 0.0);
-  Point3 Pi4(3.0, 1.0, 0.0);
-  Point3 Pi5(4.0, 1.0, 0.0);
-  Point3 Pi6(5.0, 1.0, 0.0);
-  Point3 Pi7(6.0, 1.0, 0.0);
-  Point3 Pi8(7.0, 1.0, 0.0);
-  Point3 Pi9(8.0, 1.0, 0.0);
-  Point3 Pi10(9.0, 1.0, 0.0);
-
-
-
-
-  Rot3 Ri1 = Rot3::rodriguez(0.0, 0.0, PI/2);
-  Rot3 Ri2 = Rot3::rodriguez(0.0, 0.0, PI/2);
-  Rot3 Ri3 = Rot3::rodriguez(0.0, 0.0, PI/2);
-  Rot3 Ri4 = Rot3::rodriguez(0.0, 0.0, PI/2);
-
-  float si1,si2,si3,si4;
-  si1=1;
-  si2=1;
-  si3=1;
-  si4=1;
-
-
-
-
-  // Add a prior on the first pose, setting it to the origin
-  // A prior factor consists of a mean and a noise model (covariance matrix)
-
-  Moses3 priorMean(ScSO3(s0*R0.matrix()),Pp0.vector()); // prior at origin
-  noiseModel::Diagonal::shared_ptr priorNoise = noiseModel::Diagonal::Sigmas((Vector(7) << 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01));
-  graph.add(PriorFactor<Moses3>(1, priorMean, priorNoise));
-  
-
-
 
   // 2a. Add odometry factors
   // For simplicity, we will use the same noise model for each odometry factor
-  noiseModel::Diagonal::shared_ptr odometryNoise = noiseModel::Diagonal::Sigmas((Vector(7) << 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 1));
+  auto odometryNoise = noiseModel::Diagonal::Sigmas(Vector3(0.2, 0.2, 0.1));
   // Create odometry (Between) factors between consecutive poses
-
-  graph.add(BetweenFactor<Moses3>(1, 2, Moses3(ScSO3(s12*R12.matrix()),Pp12.vector()), odometryNoise));
-  graph.add(BetweenFactor<Moses3>(2, 3, Moses3(ScSO3(s23*R23.matrix()),Pp23.vector()), odometryNoise));
-  graph.add(BetweenFactor<Moses3>(3, 4, Moses3(ScSO3(s34*R34.matrix()),Pp34.vector()), odometryNoise));
-
-  graph.add(BetweenFactor<Moses3>(4, 5, Moses3(ScSO3(s45*R45.matrix()),Pp45.vector()), odometryNoise));
-  graph.add(BetweenFactor<Moses3>(5, 6, Moses3(ScSO3(s56*R56.matrix()),Pp56.vector()), odometryNoise));
-  graph.add(BetweenFactor<Moses3>(6, 7, Moses3(ScSO3(s67*R67.matrix()),Pp67.vector()), odometryNoise));
-
-  graph.add(BetweenFactor<Moses3>(7, 8, Moses3(ScSO3(s78*R78.matrix()),Pp78.vector()), odometryNoise));
-  graph.add(BetweenFactor<Moses3>(8, 9, Moses3(ScSO3(s89*R89.matrix()),Pp89.vector()), odometryNoise));
-  graph.add(BetweenFactor<Moses3>(9, 10, Moses3(ScSO3(s90*R90.matrix()),Pp90.vector()), odometryNoise));
-
-
-
-  graphWithGPS = graph;
+  graph.emplace_shared<BetweenFactor<Pose2> >(1, 2, Pose2(2.0, 0.0, 0.0), odometryNoise);
+  graph.emplace_shared<BetweenFactor<Pose2> >(2, 3, Pose2(2.0, 0.0, 0.0), odometryNoise);
 
   // 2b. Add "GPS-like" measurements
   // We will use our custom UnaryFactor for this.
-  noiseModel::Diagonal::shared_ptr unaryNoise = noiseModel::Diagonal::Sigmas((Vector(3) << 10, 10, 10)); // 10cm std on x,y
-
-if(useUnary){
-	
-  graphWithGPS.add(boost::make_shared<GPSFactor>(1, Up1, unaryNoise));  
-  graphWithGPS.add(boost::make_shared<GPSFactor>(2, Up2, unaryNoise));
-  graphWithGPS.add(boost::make_shared<GPSFactor>(3, Up3, unaryNoise));
-  
-  graphWithGPS.add(boost::make_shared<GPSFactor>(4, Up4, unaryNoise));
-
-  graphWithGPS.add(boost::make_shared<GPSFactor>(5, Up5, unaryNoise));
-  graphWithGPS.add(boost::make_shared<GPSFactor>(6, Up6, unaryNoise));
-  graphWithGPS.add(boost::make_shared<GPSFactor>(7, Up7, unaryNoise));
-
-  graphWithGPS.add(boost::make_shared<GPSFactor>(8, Up8, unaryNoise));
-  graphWithGPS.add(boost::make_shared<GPSFactor>(9, Up9, unaryNoise));
-  graphWithGPS.add(boost::make_shared<GPSFactor>(10, Up10, unaryNoise));
-
-  cout << "GPS Points:"<<endl;
-  cout << Up1<<endl;
-  cout << Up2<<endl;
-  cout << Up3<<endl;
-  cout << Up4<<endl;
-  cout << Up5<<endl;
-  cout << Up6<<endl;
-  cout << Up7<<endl;
-  cout << Up8<<endl;
-  cout << Up9<<endl;
-  cout << Up10<<endl;
-}
-
-  //graph.add(BetweenFactor<Pose2>(1, 2, Pose2(2.0, 0.0, 0.0), odometryNoise));
-  //graph.add(BetweenFactor<Pose2>(2, 3, Pose2(2.0, 0.0, 0.0), odometryNoise));
-
-  // 2b. Add "GPS-like" measurements
-  // We will use our custom UnaryFactor for this.
-  //noiseModel::Diagonal::shared_ptr unaryNoise = noiseModel::Diagonal::Sigmas((Vector(2) << 0.1, 0.1)); // 10cm std on x,y
-  //graph.add(boost::make_shared<UnaryFactor>(1, 0.0, 0.0, unaryNoise));
-  //graph.add(boost::make_shared<UnaryFactor>(2, 2.0, 0.0, unaryNoise));
-  //graph.add(boost::make_shared<UnaryFactor>(3, 4.0, 0.0, unaryNoise));
-  
-  //graph.print("\nFactor Graph:\n"); // print
+  auto unaryNoise =
+      noiseModel::Diagonal::Sigmas(Vector2(0.1, 0.1));  // 10cm std on x,y
+  graph.emplace_shared<UnaryFactor>(1, 0.0, 0.0, unaryNoise);
+  graph.emplace_shared<UnaryFactor>(2, 2.0, 0.0, unaryNoise);
+  graph.emplace_shared<UnaryFactor>(3, 4.0, 0.0, unaryNoise);
+  graph.print("\nFactor Graph:\n");  // print
 
   // 3. Create the data structure to hold the initialEstimate estimate to the solution
   // For illustrative purposes, these have been deliberately set to incorrect values
   Values initialEstimate;
-
-  initialEstimate.insert(1, Moses3(ScSO3(si1*Ri1.matrix()),Pi1.vector()));
-  initialEstimate.insert(2, Moses3(ScSO3(si2*Ri2.matrix()),Pi2.vector()));
-  initialEstimate.insert(3, Moses3(ScSO3(si3*Ri3.matrix()),Pi3.vector()));
-  initialEstimate.insert(4, Moses3(ScSO3(si4*Ri4.matrix()),Pi4.vector()));
-
-  initialEstimate.insert(5, Moses3(ScSO3(si2*Ri2.matrix()),Pi5.vector()));
-  initialEstimate.insert(6, Moses3(ScSO3(si3*Ri3.matrix()),Pi6.vector()));
-  initialEstimate.insert(7, Moses3(ScSO3(si4*Ri4.matrix()),Pi7.vector()));
- 
-  initialEstimate.insert(8, Moses3(ScSO3(si2*Ri2.matrix()),Pi8.vector()));
-  initialEstimate.insert(9, Moses3(ScSO3(si3*Ri3.matrix()),Pi9.vector()));
-  initialEstimate.insert(10, Moses3(ScSO3(si4*Ri4.matrix()),Pi10.vector()));
-
-
-
-
-
-
-
-  //initialEstimate.insert(1, Pose2(0.5, 0.0, 0.2));
-  //initialEstimate.insert(2, Pose2(2.3, 0.1, -0.2));
-  //initialEstimate.insert(3, Pose2(4.1, 0.1, 0.1));
-  initialEstimate.print("\nInitial Estimate:\n"); // print
+  initialEstimate.insert(1, Pose2(0.5, 0.0, 0.2));
+  initialEstimate.insert(2, Pose2(2.3, 0.1, -0.2));
+  initialEstimate.insert(3, Pose2(4.1, 0.1, 0.1));
+  initialEstimate.print("\nInitial Estimate:\n");  // print
 
   // 4. Optimize using Levenberg-Marquardt optimization. The optimizer
   // accepts an optional set of configuration parameters, controlling
@@ -345,28 +148,11 @@ if(useUnary){
   Values result = optimizer.optimize();
   result.print("Final Result:\n");
 
-  LevenbergMarquardtOptimizer optimizerWithGPS(graphWithGPS, initialEstimate);
-  Values resultWithGPS = optimizerWithGPS.optimize();
-  resultWithGPS.print("FinalWithGPS Result:\n");
-
   // 5. Calculate and print marginal covariances for all variables
   Marginals marginals(graph, result);
-  Marginals marginalsWithGPS(graphWithGPS, resultWithGPS);
-
-
-if(printCov){
   cout << "x1 covariance:\n" << marginals.marginalCovariance(1) << endl;
   cout << "x2 covariance:\n" << marginals.marginalCovariance(2) << endl;
   cout << "x3 covariance:\n" << marginals.marginalCovariance(3) << endl;
-  cout << "x4 covariance:\n" << marginals.marginalCovariance(4) << endl;
 
-  cout << "x5 covariance:\n" << marginals.marginalCovariance(5) << endl;
-  cout << "x6 covariance:\n" << marginals.marginalCovariance(6) << endl;
-  cout << "x7 covariance:\n" << marginals.marginalCovariance(7) << endl;
-  cout << "x8 covariance:\n" << marginals.marginalCovariance(8) << endl;
-
-  cout << "x9 covariance:\n" << marginals.marginalCovariance(9) << endl;
-  cout << "x10 covariance:\n" << marginals.marginalCovariance(10) << endl;
-}
   return 0;
 }
